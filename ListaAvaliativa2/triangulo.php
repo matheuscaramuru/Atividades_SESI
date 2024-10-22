d3563dfe07c654250567e9bcc028db4f7810a939
MatheusCaramuru <matheus.oliveira.caramuru@gmail.com>
2024-10-22 09:45:35 -0300

    Create triangulo.php

---
+++ ListaAvaliativa2/triangulo.php
@@ -0,0 +1,11 @@
+<?php
+function calcularAreaTriangulo($base, $altura) {
+    return ($base * $altura) / 2;
+}
+
+// Exemplo de uso
+$base = 5;
+$altura = 10;
+
+echo "A área do triângulo é: " . calcularAreaTriangulo($base, $altura);
+?>
