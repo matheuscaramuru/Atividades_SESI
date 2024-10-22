9bef2baca73f8d4dfd7f2c3e83080a5a710b5000
MatheusCaramuru <matheus.oliveira.caramuru@gmail.com>
2024-10-22 09:47:34 -0300

    Create header.php

---
+++ setembro_amarelo_Sesi/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Setembro Amarelo</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="campanhas.php">Campanhas</a></li>
+                <li><a href="sobrenos.php">Sobre Nós</a></li> 
+            </ul>
+        </nav>
+    </header>
