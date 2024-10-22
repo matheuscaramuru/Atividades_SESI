fd6255ce9c07f2950756b8a09428063691d24dcd
MatheusCaramuru <matheus.oliveira.caramuru@gmail.com>
2024-10-22 09:48:09 -0300

    Create testelogin.php

---
+++ site_jacareisustentavel/testelogin.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+if(isset($_POST['submit']) && !empty($_POST['nome']) && !empty($_POST['senha']))
+{
+include_once('config.php');
+$nome = $_POST['nome'];
+$senha = $_POST['senha'];
+
+$sql = "SELECT * FROM usuarios WHERE nome = '$nome' and senha = '$senha'";
+$result = $conexao -> query($sql);
+    if(mysqli_num_rows($result) <1)
+    {
+        unset($_SESSION['nome']);
+        unset($_SESSION['senha']);
+        header("Location:login.php");
+    }
+
+else
+{
+    $_SESSION["nome"] = $nome;
+    $_SESSION["senha"] = $senha;  
+header('Location: sistema.php');
+}
+}
+else
+{
+    header('Location: login.php');
+}
+
+
+?>
