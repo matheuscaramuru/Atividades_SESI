f98ec6661fb3d14547bafa9b6f5d3739695287c6
MatheusCaramuru <matheus.oliveira.caramuru@gmail.com>
2024-10-22 09:47:39 -0300

    Create index.php

---
+++ setembro_amarelo_Sesi/index.php
@@ -0,0 +1,16 @@
+<?php include 'header.php'; ?>
+
+<main>
+<img src="setembro_amarelo_2.jpg" alt="Campanha Setembro Amarelo" class="campanha1">
+    <h1>Bem-vindo ao nosso site!</h1>
+    <section>
+        <h2>Campanha Setembro Amarelo</h2>
+        <p>A campanha Setembro Amarelo tem como objetivo promover a conscientização sobre a prevenção do suicídio e a valorização da vida. Em setembro, diversas atividades são realizadas para sensibilizar a população sobre a importância da saúde mental.</p>
+        
+    </section>
+</main>
+
+<?php include 'footer.php'; ?>
+
+</body>
+</html>
