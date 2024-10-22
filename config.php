6b32314e38a4fce16511719fc2d41bc8dbd42498
MatheusCaramuru <matheus.oliveira.caramuru@gmail.com>
2024-10-22 09:25:19 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbHost = 'LocalHost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'projeto_jacareísustentável';
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+
+?>
